b93cdb826720676a17d0eabf34843b85bfa77a12
amazingshellyyy <amazingshellyyy@gmail.com>
2020-03-18 15:23:43 -0700

    adds title and links on intro

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-import {Container} from 'react-bootstrap';
+import { Container } from 'react-bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import NavBar from './Components/NavBar/NavBar';
 import Intro from './Components/Intro/Intro';
@@ -10,16 +10,19 @@ import Projects from './Components/Projects/Projects'
 function App() {
   return (
     <>
-    <NavBar/>
-    <div style={{minHeight:"60vh", backgroundColor: "#F0F4F8"}}>
-   <Container >
-    <Intro/>
-   </Container>
-   </div>
-   <Container className="mt-5">
-    <Projects/>
-   </Container>
-   </>
+    <head>
+      <title>Amazingshellyyy</title>
+    </head>
+      {/* <NavBar/> */}
+      <div style={{ minHeight: "30vh", backgroundColor: "#F0F4F8" }}>
+        <Container >
+          <Intro />
+        </Container>
+      </div>
+      <Container className="mt-5 mb-5">
+        <Projects />
+      </Container>
+    </>
   );
 }
 
--- portfolio/src/Components/Intro/Intro.js
+++ portfolio/src/Components/Intro/Intro.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import {Row, Col} from 'react-bootstrap';
+import {IconContext} from 'react-icons';
+import {IoLogoGithub,IoLogoLinkedin,IoMdCalendar,IoIosDocument} from 'react-icons/io'
 
 const Intro = props => {
   return(
@@ -8,7 +10,12 @@ const Intro = props => {
       <Col xs={10} className="text-center">
         <h2>Shelly Cheng</h2>
         <p>I am a Software Engineer</p>
-        
+        <IconContext.Provider value={{size: "2em"}}>
+          <a href="https://github.com/amazingshellyyy"><IoLogoGithub/></a>
+          <a href="https://www.linkedin.com/in/amazingshellyyy/"><IoLogoLinkedin/></a>
+          <a href="https://calendly.com/amazingshellyyy/30min"><IoMdCalendar/></a>
+          <a href="https://amazingshellyyy.com/resume/Shelly_Cheng_Resume2020FD.pdf"><IoIosDocument/></a>
+        </IconContext.Provider>
       </Col>
       <Col xs={1}></Col>
     </Row>
