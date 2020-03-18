9427b73cf6cff9c760386ec0e1f402a11ffc7d00
amazingshellyyy <amazingshellyyy@gmail.com>
2020-03-18 15:30:08 -0700

    deploy

---
+++ deploy.sh
@@ -0,0 +1,7 @@
+cd ./portfolio
+npm run build
+git add .
+git commit -m "create docs and ready for deploy"
+git push origin master
+echo ">> frontend deployed successfully"
+echo "|| client-> https://amazingshellyyy.com/"
