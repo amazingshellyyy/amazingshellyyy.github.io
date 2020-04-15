8124d98df648e88dfb008d11d10e001b5cacfb88
amazingshellyyy <amazingshellyyy@gmail.com>
2020-04-14 17:39:25 -0700

    adjust width

--- deploy.sh
+++ deploy.sh
@@ -1,9 +1,10 @@
+
 cd ./portfolio
 npm run build
 cd ..
 cp -r ./portfolio/build/* .
-# git add .
-# git commit -m "create docs and ready for deploy"
-# git push origin master
-# echo ">> frontend deployed successfully"
-# echo "|| client-> https://amazingshellyyy.com/"
+git add .
+git commit -m "create docs and ready for deploy"
+git push origin master
+echo ">> frontend deployed successfully"
+echo "|| client-> https://amazingshellyyy.com/"
