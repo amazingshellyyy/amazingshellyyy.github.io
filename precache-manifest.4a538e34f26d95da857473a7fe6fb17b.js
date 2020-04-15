e513c0bd79263bdac000f2b1b2c9166a1bd5ba25
amazingshellyyy <amazingshellyyy@gmail.com>
2020-04-14 19:43:32 -0700

    create docs and ready for deploy

---
+++ precache-manifest.4a538e34f26d95da857473a7fe6fb17b.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "14623ae8ea49d49e682a7b85098e5155",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b738458155a0c0227754",
+    "url": "/static/css/2.de424728.chunk.css"
+  },
+  {
+    "revision": "3d4405a3afb876f2b03a",
+    "url": "/static/css/main.85dd19a3.chunk.css"
+  },
+  {
+    "revision": "b738458155a0c0227754",
+    "url": "/static/js/2.06b3f4d6.chunk.js"
+  },
+  {
+    "revision": "11a928b57aca5049fe950bf8bcde77c5",
+    "url": "/static/js/2.06b3f4d6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3d4405a3afb876f2b03a",
+    "url": "/static/js/main.c177cd9b.chunk.js"
+  },
+  {
+    "revision": "a27870e5f03a3425f6aa",
+    "url": "/static/js/runtime-main.c70048c8.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.70171b89a19c4ad875a0b670ed77dcef.js"
+  "/precache-manifest.4a538e34f26d95da857473a7fe6fb17b.js"
 );
 
 self.addEventListener('message', (event) => {
