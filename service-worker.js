1f17f662a54c069037db0962d818e323b55f5941
amazingshellyyy <amazingshellyyy@gmail.com>
2020-05-23 14:13:27 -0700

    create docs and ready for deploy

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.1fcee43f8d58f073e5cdcef268ffaf18.js"
+  "/precache-manifest.4de0956f7e8e288014de548daf08092c.js"
 );
 
 self.addEventListener('message', (event) => {
